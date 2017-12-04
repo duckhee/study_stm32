c9eaf513bc49a1de1830427ab74c77f717b70c19
duckheewon <fain9301@yahoo.com>
2017-12-04 19:22:46 +0900

    testing mango z-1

--- mangoz-1/LIB_HW/Hw_Core/Hw_EXTI.c
+++ mangoz-1/LIB_HW/Hw_Core/Hw_EXTI.c
@@ -1,11 +1,10 @@
-#define HW_EXT_IRQ_LOCAL
+#define HW_EXIT_LOCAL
 
-#include "Hw_EXT_IRQHandler.h"
+#include "Hw_EXTI.h"
 
 
-
-HW_EXT_IRQ_DEF void EXTI_Configuration(void)
-{ 
+HW_EXT_DEF void EXTI_Configuration(void)
+{
     EXTI_InitTypeDef EXTI_InitStructure;
 
     /* Configure gpio as input : Button Left-WKUP */
@@ -28,9 +27,7 @@ HW_EXT_IRQ_DEF void EXTI_Configuration(void)
     EXTI_Init(&EXTI_InitStructure);
 }
 
-
-
-HW_EXT_IRQ_DEF ITStatus EXTI_GetITStatus(uint32_t EXTI_Line)
+HW_EXT_DEF ITStatus EXTI_GetITStatus(uint32_t EXTI_Line)
 {
   ITStatus bitstatus = RESET;
   uint32_t enablestatus = 0;
@@ -46,12 +43,12 @@ HW_EXT_IRQ_DEF ITStatus EXTI_GetITStatus(uint32_t EXTI_Line)
   return bitstatus;
 }
 
-HW_EXT_IRQ_DEF void EXTI_ClearITPendingBit(uint32_t EXTI_Line)
+HW_EXT_DEF void EXTI_ClearITPendingBit(uint32_t EXTI_Line)
 {
   EXTI->PR = EXTI_Line;
 }
 
-HW_EXT_IRQ_DEF void EXTI0_IRQHandler(void)
+HW_EXT_DEF void EXTI0_IRQHandler(void)
 {
     if(EXTI_GetITStatus(GPIO_EXTI_Line_KEY1) != RESET) {
         EXTI_ClearITPendingBit(GPIO_EXTI_Line_KEY1);
@@ -59,36 +56,36 @@ HW_EXT_IRQ_DEF void EXTI0_IRQHandler(void)
     }
 }
 
-HW_EXT_IRQ_DEF void EXTI1_IRQHandler(void)
+HW_EXT_DEF void EXTI1_IRQHandler(void)
 {
-    if(EXTI_GetITStatus(GPIO_EXTI_Line_KEY2) != RESET) {
+   if(EXTI_GetITStatus(GPIO_EXTI_Line_KEY2) != RESET) {
         EXTI_ClearITPendingBit(GPIO_EXTI_Line_KEY2);
         printf("Right-USER Button Press\n");
     }
 }
 
-
-HW_EXT_IRQ_DEF void GPIO_EXTILineConfig(uint8_t GPIO_PortSource, uint8_t GPIO_PinSource)
+HW_EXT_DEF void GPIO_EXTILineConfig(uint8_t GPIO_PortSource, uint8_t GPIO_PinSource)
 {
   uint32_t tmp = 0x00;
-  
+
   tmp = ((uint32_t)0x0F) << (0x04 * (GPIO_PinSource & (uint8_t)0x03));
   AFIO->EXTICR[GPIO_PinSource >> 0x02] &= ~tmp;
   AFIO->EXTICR[GPIO_PinSource >> 0x02] |= (((uint32_t)GPIO_PortSource) << (0x04 * (GPIO_PinSource & (uint8_t)0x03)));
+
 }
 
-HW_EXT_IRQ_DEF void EXTI_Init(EXTI_InitTypeDef* EXTI_InitStruct)
+HW_EXT_DEF void EXTI_Init(EXTI_InitTypeDef* EXTI_InitStruct)
 {
   uint32_t tmp = 0;
 
   tmp = (uint32_t)EXTI_BASE;
-     
+
   if (EXTI_InitStruct->EXTI_LineCmd != DISABLE)
   {
     /* Clear EXTI line configuration */
     EXTI->IMR &= ~EXTI_InitStruct->EXTI_Line;
     EXTI->EMR &= ~EXTI_InitStruct->EXTI_Line;
-    
+
     tmp += EXTI_InitStruct->EXTI_Mode;
 
     *(__IO uint32_t *) tmp |= EXTI_InitStruct->EXTI_Line;
@@ -96,7 +93,7 @@ HW_EXT_IRQ_DEF void EXTI_Init(EXTI_InitTypeDef* EXTI_InitStruct)
     /* Clear Rising Falling edge configuration */
     EXTI->RTSR &= ~EXTI_InitStruct->EXTI_Line;
     EXTI->FTSR &= ~EXTI_InitStruct->EXTI_Line;
-    
+
     /* Select the trigger for the selected external interrupts */
     if (EXTI_InitStruct->EXTI_Trigger == EXTI_Trigger_Rising_Falling)
     {
@@ -108,16 +105,14 @@ HW_EXT_IRQ_DEF void EXTI_Init(EXTI_InitTypeDef* EXTI_InitStruct)
     {
       tmp = (uint32_t)EXTI_BASE;
       tmp += EXTI_InitStruct->EXTI_Trigger;
-
       *(__IO uint32_t *) tmp |= EXTI_InitStruct->EXTI_Line;
     }
   }
   else
   {
     tmp += EXTI_InitStruct->EXTI_Mode;
-
     /* Disable the selected external lines */
     *(__IO uint32_t *) tmp &= ~EXTI_InitStruct->EXTI_Line;
   }
-}
 
+}
---
+++ mangoz-1/LIB_HW/Hw_Core/Hw_EXTI.h
@@ -0,0 +1,29 @@
+#ifndef __HW_EXTI_H__
+#define __HW_EXTI_H__
+
+#include "Hw.h"
+
+#ifdef HW_EXT_LOCAL
+#define HW_EXT_DEF
+#else
+#define HW_EXT_DEF                        extern
+#endif
+
+#define GPIO_PORTSOURCE_KEY        GPIO_PortSourceGPIOA
+#define GPIO_PINSOURCE_KEY1        GPIO_PinSource0
+#define GPIO_PINSOURCE_KEY2        GPIO_PinSource1
+#define GPIO_EXTI_Line_KEY1        EXTI_Line0
+#define GPIO_EXTI_Line_KEY2        EXTI_Line1
+#define AIRCR_VECTKEY_MASK    ((uint32_t)0x05FA0000)
+
+
+HW_EXT_DEF void EXTI_Configuration(void);
+HW_EXT_DEF ITStatus EXTI_GetITStatus(uint32_t EXTI_Line);
+HW_EXT_DEF void EXTI_ClearITPendingBit(uint32_t EXTI_Line);
+HW_EXT_DEF void EXTI0_IRQHandler(void);
+HW_EXT_DEF void EXTI1_IRQHandler(void);
+HW_EXT_DEF void GPIO_EXTILineConfig(uint8_t GPIO_PortSource, uint8_t GPIO_PinSource);
+HW_EXT_DEF void EXTI_Init(EXTI_InitTypeDef* EXTI_InitStruct);
+
+
+#endif
