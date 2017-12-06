4556704ef2e37e86ec7fd3ecbda4717e3ca5ef64
duckhee <fain9301@yahoo.com>
2017-12-06 22:07:27 +0900

    tim c file add

---
+++ mangom-3/LIB_HW/Hw_Core/Hw_Tim.c
@@ -0,0 +1,4 @@
+#define HW_TIM_LOCAL
+
+#include "Hw_Tim.h"
+
---
+++ mangom-3/LIB_HW/Hw_Core/Hw_Tim.h
@@ -0,0 +1,15 @@
+#ifndef __HW_TIM_H__
+#define __HW_TIM_H__
+
+#include "Hw.h"
+
+#ifdef HW_TIM_LOCAL
+#define TIM_DEF
+#else
+#define TIM_DEF                    extern
+#endif
+
+
+
+
+#endif
